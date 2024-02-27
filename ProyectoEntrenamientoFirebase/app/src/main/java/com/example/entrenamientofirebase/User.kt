1c4eaecd7cffcd1f808f02887c53b46eebc2afe3
Víctor Galiano <victoradonay.galianopombo@gmail.com>
2024-02-27 19:19:28 +0100

    yass

---
+++ ProyectoEntrenamientoFirebase/app/src/main/java/com/example/entrenamientofirebase/User.kt
@@ -0,0 +1,8 @@
+package com.example.entrenamientofirebase
+
+data class User(
+    var firstName: String? = null,
+    var lastName: String? = null,
+    var age: String? = null,
+    var userName: String? = null
+)
